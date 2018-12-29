e202c205375ed5c711eb86f59fe804d1a4b03dfd
Alexei Figueroa <alexeifigueroa@gmail.com>
2018-12-29 13:00:27 +0100

    Fix bug in nested function that draws arcs/circles Add comments on sources in exploration.R

--- exploration.R
+++ exploration.R
@@ -1,6 +1,9 @@
 shots<-read.csv('data/data.csv')
 source("plotCourt.R")
-plot.court(55.5)
+# 55.5 is the y offset in inches of the court
+# corresponds to the center of the basket ring.
+plot.court()
+
 points(shots[shots$shot_made_flag == 1,]$loc_x , 
        shots[shots$shot_made_flag == 1,]$loc_y , 
        pch ='.',
